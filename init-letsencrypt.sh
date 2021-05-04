b689ba34fb26ec0ad643337d62d1fb1a675ca30c
Andrey Sergeevich <andrey.s.h.68@yandex.ru>
2021-05-04 16:29:36 +0300

    cayman

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(89204897437.ru www.89204897437.ru)
+domains=(cayman-store.ru www.cayman-store.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="gabrial187@gmail.com" # Adding a valid address is strongly recommended
